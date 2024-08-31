d7b2270aef0c1ed48c92a0b8883b907ef446654a
jsanchezba <13497454+jsanchezba@users.noreply.github.com>
2024-08-31 23:05:09 +0200

    fix emit RPC with process

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -4,13 +4,15 @@ class_name Player extends CharacterBody2D
 const speed = 300.0
 var current_delta
 
-func _enter_tree() -> void:
-	set_multiplayer_authority(str(name).to_int())
+#func _enter_tree() -> void:
+	#set_multiplayer_authority(str(name).to_int())
 	
 func _ready() -> void:
+	set_multiplayer_authority(str(name).to_int())
 	if name:
 		player_name.text = str(name)
-		
+
+func _process(delta: float) -> void:
 	if is_multiplayer_authority():
 		update_player_position.rpc(name, position)
 
@@ -24,8 +26,8 @@ func _physics_process(delta: float) -> void:
 		move_and_slide()
 
 @rpc('any_peer', 'call_local', 'unreliable')
-func update_player_position(id: int, updated_position: Vector2):
+func update_player_position(entity_id: String, updated_position: Vector2):
 	if not is_multiplayer_authority():
-		if name.to_int() == id:
+		if name == entity_id:
 			position = lerp(position, updated_position, current_delta * 15)
 	
