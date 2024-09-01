b9bcacb96982c5d5091c86f1704d64788ac72471
jsanchezba <13497454+jsanchezba@users.noreply.github.com>
2024-09-01 19:30:08 +0200

    sync direction

--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -7,6 +7,7 @@ var current_delta
 
 var id: int = 0
 var username: String = ''
+var player_direction: Vector2
 #func _enter_tree() -> void:
 	#set_multiplayer_authority(str(name).to_int())
 	
@@ -16,24 +17,26 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if is_multiplayer_authority():
-		update_player_position.rpc(name, position)
+		update_player_position.rpc(name, position, player_direction)
 
 func _physics_process(delta: float) -> void:
 	current_delta = delta
 	if is_multiplayer_authority():
 		var direction = Input.get_vector('left', 'right', 'up', 'down')
-		
+		player_direction = direction
 		velocity = direction * speed
-		if direction:
-			sprite.flip_h = true if direction.x == -1 else false
-			sprite.play('run')
-		else:
-			sprite.play('idle')
 		move_and_slide()
+		
+	if player_direction:
+		sprite.flip_h = true if player_direction.x == -1 else false
+		sprite.play('run')
+	else:
+		sprite.play('idle')
 
 @rpc('any_peer', 'call_local', 'unreliable')
-func update_player_position(entity_id: String, updated_position: Vector2):
+func update_player_position(entity_id: String, updated_position: Vector2, updated_direction: Vector2):
 	if not is_multiplayer_authority():
 		if name == entity_id:
 			position = lerp(position, updated_position, current_delta * 15)
+			player_direction = updated_direction
 	
